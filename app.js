5103d0e7897f34e1b4348db8bc472590941a9881
Leon <wanliangwang@163.com>
2018-04-09 16:46:37 +0800

    增加热门城市选择

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 //app.js
 App({
-  onLaunch() {
-    console.log('launch')
-  },
+    onLaunch() {
+        console.log('launch')
+    },
 
-  onShow(opth){
-    console.log('show')
-  },
+    onShow(opth) {
+        console.log('show')
+    },
 
-  onHide(){
-    console.log('hide')
-  },
-  onError(){
-    console.log('error')
-  },
-  globalData: {
-    weather: null,
-    hourly: null,
-    inputCity: ''
-  }
+    onHide() {
+        console.log('hide')
+    },
+    onError() {
+        console.log('error')
+    },
+    globalData: {
+        weather: null,
+        hourly: null,
+        inputCity: ''
+   
+    }
 })
