83195810a7d1a8f89d0a48d3dc371548347cdbe9
Leon <wanliangwang@163.com>
2018-04-09 16:53:19 +0800

    修改近期天气获取bug,将调用函数放在onShow中

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -13,26 +13,25 @@ Page({
 
 
   onShow () {
-    console.log('show')
+    var _this = this
+    console.log('load')
+    if (app.globalData.weather) {
+      _this.setData({ weather: app.globalData.weather })
+      return
+    }
+
+
+    API.getWeather(app.globalData.inputCity)
+      .then((weather) => {
+        weather.format_last_update = Util.formatTime(weather.last_update)
+        weather.bg = Util.getBackground(weather.now.code)
+        _this.setData({ weather })
+        app.globalData.weather = weather
+      }).catch(_this.onError)
   },
 
   onLoad (options){
-    var _this = this
-    console.log('load')
-    if(app.globalData.weather){
-      _this.setData({weather: app.globalData.weather})
-      return
-    }
     
-    API.getLocation()
-      .then(API.getCityId)
-      .then(API.getNowWeather)
-      .then((weather)=>{
-        weather.format_last_update = Util.formatTime(weather.last_update)
-        weather.bg = Util.getBackground(weather.now.code)
-        _this.setData({weather})
-        app.globalData.weather = weather
-      }).catch(_this.onError)
   },
 
   onError(err){
--- pages/index/index.js
+++ pages/index/index.js
@@ -14,27 +14,7 @@ Page({
 
     onLoad(options) {
         // console.log('load888')
-        // let _this = this
-        // API.getCityId()
-        //     .then((cityid) => {
-        //         API.getNowWeather(cityid)
-        //             .then((weather) => {
-        //                 weather.format_last_update = Util.formatTime(weather.last_update)
-        //                 weather.bg = Util.getBackground(weather.now.code)
-        //                 _this.setData({weather})
-        //                 app.globalData.weather = weather
-        //             }).catch(_this.onError)
-        //
-        //         API.get24hWeather(cityid)
-        //             .then((hourly) => {
-        //                 hourly.forEach((hour) => {
-        //                     hour.img = `../../images/weather/${hour.code}.png`
-        //                     hour.format_time = Util.formatHour(hour.time)
-        //                 })
-        //                 _this.setData({hourly})
-        //                 app.globalData.hourly = hourly
-        //             }).catch(_this.onError)
-        //     }).catch(_this.onError)
+       
     },
     onShow() {
         console.log('show666')
